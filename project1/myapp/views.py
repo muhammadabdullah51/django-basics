1f20b01974a77ea372610b19a1b7e43a0c69bb68
muhammadabdullah51 <muhabd51@gmail.com>
2023-11-18 20:29:13 +0500

    added openapi

--- project1/myapp/views.py
+++ project1/myapp/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from .models import Person
 # Create your views here.
 
+from openai import OpenAI
+client = OpenAI(api_key='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')
+
+
+def gpt_turbo(strval):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+)
+
+    return str(completion.choices[0].message.content)
+
 def welcome(request):
     return render(request, 'index.html')
 
@@ -20,4 +35,17 @@ def contactus(request):
     return render(request, 'contact.html', {'result' : result})
 
 def generate(request):
-    return render(request, 'generate.html')  
+    results = None
+    print("hi")
+    if request.method=='POST':
+
+        user_input_text=str(request.POST['text'])
+        try:
+                
+            gpt_output = gpt_turbo(user_input_text)
+            results = gpt_output
+            myinstaance = Person(userinputvalue = user_input_text, mycalvalue = results)
+            myinstaance.save()
+        except:
+            pass
+    return render(request, 'generate.html', {'result' : results})  
